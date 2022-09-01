de38386438290c1945ed0b40919fd5dcdd593e41
Steve Godin <steve.god@gmail.com>
2022-09-01 07:35:38 -0700

    Add tests for and start of subscribe endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,29 @@
 use std::{io, net::TcpListener};
 
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse, dev::Server};
+use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
 
     Ok(server)
 }
-
-// Page 28
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,17 @@
 //! tests/health_check.rs
-
 use std::net::TcpListener;
+
+/// Spin up an instance of our application
+/// and returns its address (i.e. http://localhost:XXXX)
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{port}")
+}
 
 // `tokio::test` is the testing equivalent of `tokio::main`.
 // It also spares you from having to specify the `#[test]` attribute.
@@ -10,21 +21,58 @@ use std::net::TcpListener;
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
-
     let client = reqwest::Client::new();
 
-    let response = client.get(&format!("{address}/health_check")).send().await.expect("Failed to execute request.");
+    let response = client
+        .get(&format!("{address}/health_check"))
+        .send()
+        .await
+        .expect("Failed to execute request.");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
 
-    let _ = tokio::spawn(server);
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{address}/subscriptions"))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
 
-    format!("http://127.0.0.1:{port}")
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{address}/subscriptions"))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 when the payload was {error_message}"
+        )
+    }
 }
