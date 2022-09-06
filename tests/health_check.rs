41a6f26d8aa7009878bee06bddff3bc38a3ee4b6
Steve Godin <steve.god@gmail.com>
2022-09-05 20:17:11 -0700

    Check db persistences in subscribe test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,9 @@
 //! tests/health_check.rs
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
+use zero2prod::configuration::get_configuration;
+
 /// Spin up an instance of our application
 /// and returns its address (i.e. http://localhost:XXXX)
 fn spawn_app() -> String {
@@ -36,6 +39,10 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let mut connection = PgConnection::connect(&configuration.database.url)
+        .await
+        .expect("Failed to connect to Postgres");
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -48,6 +55,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscriptions");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
