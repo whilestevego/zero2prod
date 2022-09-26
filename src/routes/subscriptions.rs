531d60db9d1079c1a0c384e11155ef908bf78105
Steve Godin <steve.god@gmail.com>
2022-09-26 07:20:16 -0700

    Add basic logs to subscribe

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,6 +9,12 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    log::info!(
+        "Adding '{}' '{}' as a new subscriber.",
+        form.email,
+        form.name
+    );
+
     match sqlx::query!(
         r#"
             INSERT INTO subscriptions (email, name, subscribed_at)
@@ -21,9 +27,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("New subscriber details have been saved");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {e}");
+            log::error!("Failed to execute query: {e}");
             HttpResponse::InternalServerError().finish()
         }
     }
