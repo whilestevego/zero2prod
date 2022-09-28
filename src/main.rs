41672edb38e1eca0ffce030ff1f1da5a6de057fc
Steve Godin <steve.god@gmail.com>
2022-09-28 06:59:03 -0700

    Setup tracing registry

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::dispatcher::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::{
     configuration::{get_configuration, Settings},
     startup::run,
@@ -8,7 +11,17 @@ use zero2prod::{
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    LogTracer::init().expect("Failed to set logger");
+
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    set_global_default(subscriber.into()).expect("Failed to set subscriber");
 
     let Settings {
         application_port,
