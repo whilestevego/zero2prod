44053b7f5d3a82fa05b666946731bbe275386216
Steve Godin <steve.god@gmail.com>
2022-10-07 10:23:29 -0700

    Use db_pool from spawn_app

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -100,11 +100,7 @@ async fn health_check_works() {
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    let TestApp { address, .. } = spawn_app().await;
-    let configuration = get_configuration().expect("Failed to read configuration");
-    let mut connection = PgConnection::connect(configuration.database.url.expose_secret())
-        .await
-        .expect("Failed to connect to Postgres");
+    let TestApp { address, db_pool } = spawn_app().await;
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -119,7 +115,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(200, response.status().as_u16());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-        .fetch_one(&mut connection)
+        .fetch_one(&db_pool)
         .await
         .expect("Failed to fetch saved subscriptions");
 
