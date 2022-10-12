ba9a1510fa41c249a3ce33e71f9ac21ea3dcc510
Steve Godin <steve.god@gmail.com>
2022-10-12 15:28:36 -0700

    Test SubscriberEmail::parse against random valid emails

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -22,21 +22,31 @@ impl AsRef<str> for SubscriberEmail {
 #[cfg(test)]
 mod tests {
     use super::SubscriberEmail;
-    use claims::assert_err;
+    use claims::{assert_err, assert_ok};
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
 
     #[test]
     fn empty_string_is_rejected() {
         let email = "".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
+
     #[test]
     fn email_missing_at_symbol_is_rejected() {
         let email = "ursuladomain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
+
     #[test]
     fn email_missing_subject_is_rejected() {
         let email = "@domain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn valid_emails_are_parsed_successfully() {
+        let email = SafeEmail().fake();
+        assert_ok!(SubscriberEmail::parse(email));
     }
 }
