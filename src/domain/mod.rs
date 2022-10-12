c9bcc8af7e95ca678d5ccf8b671a984d3da5e54d
Steve Godin <steve.god@gmail.com>
2022-10-12 15:19:07 -0700

    Implement SubscriberEmail

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,5 +1,6 @@
 mod new_subscriber;
 mod subscriber_name;
+mod subscriber_email;
 
 pub use new_subscriber::NewSubscriber;
 pub use subscriber_name::SubscriberName;
--- src/domain/subscriber_name.rs
+++ src/domain/subscriber_name.rs
@@ -27,7 +27,7 @@ impl AsRef<str> for SubscriberName {
 
 #[cfg(test)]
 mod tests {
-    use crate::domain::SubscriberName;
+    use super::SubscriberName;
     use claims::{assert_err, assert_ok};
 
     #[test]
