b46db78dca2cdc5d7d83f01012ea41d16b6290f7
Steve Godin <steve.god@gmail.com>
2023-01-11 17:38:15 -0300

    Validate login credentials

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,14 +1,34 @@
 use actix_web::{http::header::LOCATION, web, HttpResponse};
 use secrecy::Secret;
+use sqlx::PgPool;
+
+use crate::authentication::{validate_credentials, Credentials};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
     username: String,
-    paswword: Secret<String>,
+    password: Secret<String>,
 }
 
-pub async fn login(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::SeeOther()
-        .insert_header((LOCATION, "/"))
-        .finish()
+#[tracing::instrument(skip(form, db_pool), fields(username=tracing::field::Empty, user_id=tracing::field::Empty))]
+pub async fn login(form: web::Form<FormData>, db_pool: web::Data<&PgPool>) -> HttpResponse {
+    let credentials = Credentials {
+        username: form.0.username,
+        password: form.0.password,
+    };
+
+    tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
+
+    match validate_credentials(&db_pool, credentials).await {
+        Ok(user_id) => {
+            tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+
+            HttpResponse::SeeOther()
+                .insert_header((LOCATION, "/"))
+                .finish()
+        }
+        Err(_) => {
+            todo!()
+        }
+    }
 }
