1aa9cdefc971d86d1d9f79c0e8584a3c10b97c58
Steve Godin <steve.god@gmail.com>
2023-01-12 08:15:40 -0300

    Implement LoginError

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,8 +1,14 @@
-use actix_web::{http::header::LOCATION, web, HttpResponse};
+use std::fmt::Debug;
+
+use actix_web::{http::header::LOCATION, web, HttpResponse, ResponseError};
+use reqwest::StatusCode;
 use secrecy::Secret;
 use sqlx::PgPool;
 
-use crate::authentication::{validate_credentials, Credentials};
+use crate::{
+    authentication::{validate_credentials, AuthError, Credentials},
+    error_chain_fmt,
+};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -11,7 +17,10 @@ pub struct FormData {
 }
 
 #[tracing::instrument(skip(form, db_pool), fields(username=tracing::field::Empty, user_id=tracing::field::Empty))]
-pub async fn login(form: web::Form<FormData>, db_pool: web::Data<&PgPool>) -> HttpResponse {
+pub async fn login(
+    form: web::Form<FormData>,
+    db_pool: web::Data<&PgPool>,
+) -> Result<HttpResponse, LoginError> {
     let credentials = Credentials {
         username: form.0.username,
         password: form.0.password,
@@ -19,16 +28,39 @@ pub async fn login(form: web::Form<FormData>, db_pool: web::Data<&PgPool>) -> Ht
 
     tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
 
-    match validate_credentials(&db_pool, credentials).await {
-        Ok(user_id) => {
-            tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+    let user_id = validate_credentials(&db_pool, credentials)
+        .await
+        .map_err(|e| match e {
+            AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
+            AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
+        })?;
 
-            HttpResponse::SeeOther()
-                .insert_header((LOCATION, "/"))
-                .finish()
-        }
-        Err(_) => {
-            todo!()
+    tracing::Span::current().record("user.id", &tracing::field::display(&user_id));
+
+    Ok(HttpResponse::SeeOther()
+        .insert_header((LOCATION, "/"))
+        .finish())
+}
+
+#[derive(thiserror::Error)]
+pub enum LoginError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error("Something went wrong")]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl Debug for LoginError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for LoginError {
+    fn status_code(&self) -> reqwest::StatusCode {
+        match self {
+            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
         }
     }
 }
