5c85efaaabcd654b17c5c48a966ee2bb859acc3e
Steve Godin <steve.god@gmail.com>
2023-01-15 11:42:19 -0300

    Add form tag and style it

--- src/views/login.rs
+++ src/views/login.rs
@@ -12,18 +12,20 @@ pub fn post() -> Markup {
 
 pub fn form() -> Markup {
     html! {
-        label {
-            "Username "
-            input type="text" placeholder="Enter Username" name="username";
+        form action="/login" method="post" {
+            div style="display: flex; flex-direction: column; align-items: flex-start; gap: 1rem;" {
+                label {
+                    "Username "
+                    input type="text" placeholder="Enter Username" name="username";
+                }
+
+                label {
+                    "Password "
+                    input type="password" placeholder="Enter Password" name="password";
+                }
+
+                button type="submit" { "Login" }
+            }
         }
-
-        br;
-
-        label {
-            "Password "
-            input type="password" placeholder="Enter Password" name="password";
-        }
-
-        button type="submit" { "Login" }
     }
 }
